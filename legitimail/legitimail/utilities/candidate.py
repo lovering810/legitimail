cbfad167adb3cd5d5f785a0fd12335d54d46db1d
Rebecca Lovering <lovering810@gmail.com>
2024-01-11 12:41:00 -0500

    candidate parent class

---
+++ legitimail/legitimail/utilities/candidate.py
@@ -0,0 +1,30 @@
+import re
+import logging
+
+
+class Candidate:
+
+    def __init__(self, entry: str):
+        if not self.validate_string(entry=entry):
+            raise AttributeError()
+        self.entry = entry
+        self.name, self.domain = self.entry.spllit("@")
+        self.results = []
+
+    @classmethod
+    def from_string(cls, entry: str):
+        try:
+            return Candidate(entry=entry)
+        except AttributeError:
+            logging.debug(f"Submitted entry ({entry}) invalid")
+
+    @staticmethod
+    def validate_string(entry: str):
+        # regex for syntax - ganked, not generated, probably very rudimentary
+        email_regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+        return re.fullmatch(email_regex, entry)
+
+    def assess(self):
+        # this is where we look at what the filters have given us back and
+        # determine if this is a Real Person
+        raise NotImplementedError()
