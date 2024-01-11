16d7c698917da6b697e37d054152e118404663fe
Rebecca Lovering <lovering810@gmail.com>
2024-01-11 12:51:14 -0500

    add unhappy path test

--- legitimail/tests/test_filters.py
+++ legitimail/tests/test_filters.py
@@ -1,6 +1,7 @@
 from legitimail.filters.domain import Domain
 from legitimail.utilities.candidate import Candidate
 import pytest
+from requests.exceptions import ConnectionError
 
 
 @pytest.fixture
@@ -10,8 +11,19 @@ def domain():
 
 @pytest.fixture
 def good_candidate():
-    return Candidate(entry="lovering810@gmail.com")
+    return Candidate(entry="fakeyMcfakerson@gmail.com")
 
 
-def test_happy_candidate_domain(domain, good_candidate):
+@pytest.fixture
+def nonesuch_domain():
+    return Candidate(entry="fakeyMcfakerson@fake.realbad")
+
+
+def test_happy_domain(domain, good_candidate):
     assert domain.filter(candidate=good_candidate)
+
+
+def test_bad_domain(domain, nonesuch_domain):
+
+    with pytest.raises(ConnectionError):
+        domain.filter(candidate=nonesuch_domain)
